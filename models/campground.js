a70599939360236ee448230ee12e61193261899c
Nishtha Garg <33648893+nishtha-garg@users.noreply.github.com>
2018-02-20 19:09:07 -0600

    Add files via upload
    final updated models-campgrounds, users, comments

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,20 @@ var mongoose=require("mongoose");
 var campgroundSchema=new mongoose.Schema({
     name:String,
     image:String,
+    price:String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    createdAt: { type: Date, default: Date.now },
     description:String,
+     author: 
+    {
+        id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+        },
+        username: String
+    },
     comments:[
         {
             type:mongoose.Schema.Types.ObjectId,
@@ -12,7 +25,7 @@ var campgroundSchema=new mongoose.Schema({
         }
         ]
     
-});
+},{ usePushEach: true });
 
 //modelling
 module.exports=mongoose.model("Campground",campgroundSchema);
--- models/comment.js
+++ models/comment.js
@@ -3,9 +3,17 @@ var mongoose=require("mongoose");
 //Schema Setup
 var commentSchema=new mongoose.Schema({
     text:String,
-    author:String,
+    author: 
+    {
+        id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+        },
+        createdAt: { type: Date, default: Date.now },
+        username: String
+    }
     
-});
+},{ usePushEach: true });
 
 //modelling
 module.exports=mongoose.model("Comment",commentSchema);
